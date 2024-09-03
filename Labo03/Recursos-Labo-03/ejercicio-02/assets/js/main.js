649b2bbfe5cb11a386c64a6154ef656a67d781ca
RafaGdelB23 <00073123@uca.edu.sv>
2024-09-03 12:18:55 -0600

    Ejercicio02 terminado

--- Labo03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
+++ Labo03/Recursos-Labo-03/ejercicio-02/assets/js/main.js
@@ -1,53 +1,53 @@
-
 const plus = (a, b) => {
-    var sum = a + b;
-    return sum;
+  var sum = a + b;
+    alert('El resultado de la suma es: ' + sum);
 };
-const subtract = (a, b) =>{
-    var rest = a - b;
-    return rest;
-} ;
-const multiply = (a, b) =>{
-    var mult = a * b;
-    return mult;
-} ;
+const subtract = (a, b) => {
+  var rest = a - b;
+  alert('El resultado de la resta es: ' + rest);
+};
+const multiply = (a, b) => {
+  var mult = a * b;
+  alert('El resultado de la multiplicación es: ' + mult);
+};
 const split = (a, b) => {
-    var div = a / b;
-    return div;
-};
-const requestNumber = () => {
-    var a = parseInt(prompt("Ingrese un número: "))
-    var b = parseInt(prompt("Ingrese otro número: "))
-    return a,b;
+  var div = a / b;
+  alert('El resultado de la división es: ' + div);
 };
 
-const selectOperation = () => {
-    var opcion = prompt("Ingrese la operación que le gustaría realizar: ")
-    switch(opcion){
-        case '1':
-            plus(sum);
-            alert(plus);
-            break;
-        case '2':
-            subtract(rest);
-            alert(subtract);
-            break;
-        case '3':
-            multiply(multiply);
-            alert(multiply);
-            break;
-        case '4':
-            split(split);
-            alert(split);
-            break;
-        default:
-            alert('Escoja una opción valida');
-    }
+const requestNumber = () => {
+  var a = parseInt(prompt("Ingrese un número: "));
+  var b = parseInt(prompt("Ingrese otro número: "));
+  return [a, b];
+};
+
+const selectOperation = (a, b) => {
+  const opcion = prompt(
+    "Ingrese la operación que le gustaría realizar:\n1. Suma\n2. Resta\n3. Multiplicación\n4. División"
+  );
+  switch (opcion) {
+    case "1":
+      resultado = plus(a, b);
+      break;
+    case "2":
+      resultado = subtract(a, b);
+      break;
+    case "3":
+      resultado = multiply(a, b);
+      break;
+    case "4":
+      resultado = split(a, b);
+      break;
+    default:
+      alert("Escoja una opción valida");
+      return;
+  }
+
 };
 
 const main = () => {
-    requestNumber();
-    selectOperation();
-}
+  const [a, b] = requestNumber();
+  selectOperation(a, b);
+};
 
 main();
